b426195081811d7c8ea7ea8bf72733701bb7a6b1
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-06-20 06:44:40 -0400

    adicionesSumaCadenas

---
+++ src/sumaCadena.js
@@ -0,0 +1,43 @@
+class CalcuCadenas {
+    calcularSumaCadenas(cadena) {
+      if (cadena.length === 0) {
+        return 0;
+      }
+  
+      if (cadena.includes("-")) {
+        cadena = cadena.replace("-", ",");
+      }
+  
+      const numeros = cadena.split(",");
+      const suma = numeros.reduce((total, numero) => {
+        return total + Number(numero);
+      }, 0);
+  
+      return suma;
+    }
+  }
+  
+  describe("CalcuCadenas", () => {
+    it("Debería retornar la suma de los números separados por coma y guion", () => {
+      const calcular = new CalcuCadenas();
+      const cadena = "1-2,3";
+      const resultado = calcular.calcularSumaCadenas(cadena);
+      expect(resultado).toEqual(6);
+    });
+  
+    it("Debería retornar 0 para una cadena vacía", () => {
+      const calcular = new CalcuCadenas();
+      const cadena = "";
+      const resultado = calcular.calcularSumaCadenas(cadena);
+      expect(resultado).toEqual(0);
+    });
+  
+    it("Debería retornar el número mismo para una cadena que contiene un solo número", () => {
+      const calcular = new CalcuCadenas();
+      const cadena = "5";
+      const resultado = calcular.calcularSumaCadenas(cadena);
+      expect(resultado).toEqual(5);
+    });
+  
+    // Agrega más pruebas según sea necesario
+  });
